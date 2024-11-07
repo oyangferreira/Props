529daba09322a4081b8c16563038ebc3f0482c54
oyangferreira <yang.ferreira2023@gmail.com>
2024-11-07 16:36:41 -0300

    first-commit

---
+++ App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import { View, Text, Image } from 'react-native'; 
+
+class App extends Component{
+  render(){
+
+    let nome = 'Yang';
+    let img = 'https://sujeitoprogramador.com/steve.png'
+
+    return(
+      <View>
+        <Text>Hello Word!</Text>
+        <Text>Meu Primeiro App</Text>    
+        <Text style={{ color: '#FF0000', fontSize: 25, margin: 15 }}>
+          Test
+        </Text>
+
+        <Image 
+          source={{ uri: img }}
+          style={{ width: 300, height: 300 }}
+        />
+
+        <Text style={{ fontSize: 30 }}> {nome} </Text>
+
+      </View>
+    );
+  }
+}
+
+export default App;
